bc8219bd3bd4ce8706efd607a5917625429cb97e
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-16 00:54:17 -0800

    syntax fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-
-int main (void) {
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void) {
 	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	fwrite(string, sizeof(string) - 1, 1, stdout);
 	return(1);
-};
+}
