c724770c274fd0435395099540793353ff3b1a10
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-16 01:20:04 -0800

    syntax fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 	{
-		char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		char string[] 
+			= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 		fwrite(string, sizeof(string) - 1, 1, stdout);
 		return (1);
 	}
