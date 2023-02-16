7634c8ff2164b27ae4a0a8a5087957eaa20124e2
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-16 01:07:21 -0800

    syntax fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void) {
-	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fwrite(string, sizeof(string) - 1, 1, stdout);
-	return(1);
-}
+/*
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+	{
+		char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+		fwrite(string, sizeof(string) - 1, 1, stdout);
+		return (1);
+	}
