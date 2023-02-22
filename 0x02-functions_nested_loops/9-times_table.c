4c4a01a759ca5410f07fd8d11dc5c386da4be22d
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-21 16:25:46 -0800

    fix 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,13 @@ void times_table(void)
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
 			}
-			else
+			else if (b != 0)
 			{
 				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
 				_putchar(c + '0');
 			}
 
