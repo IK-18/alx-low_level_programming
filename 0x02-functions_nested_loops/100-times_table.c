cd252dcbb259bf2d6f5ca563deccd220c1566cee
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-21 18:30:59 -0800

    fix 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,23 +21,27 @@ void print_times_table(int n)
 		while (b <= n)
 		{
 			c = a * b;
-			if (c > 99)
+			if (c > 99){
 				_putchar(c / 100 + '0');
 				_putchar((c / 10 % 10) + '0');
 				_putchar(c % 10 + '0');
-			else if (c > 9)
+			}
+			else if (c > 9){
 				_putchar(' ');
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
-			else if (b != 0)
+			}
+			else if (b != 0){
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
+			}
 			else
 				_putchar(c + '0');
-			if (b != n)
+			if (b != n){
 				_putchar(',');
 				_putchar(' ');
+			}
 			b++;
 		}
 		_putchar('\n');
