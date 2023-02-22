b542609b4e311e4edf9c0cb8f46bdcc5aff170ef
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-21 18:33:32 -0800

    fix 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,37 +8,38 @@
 
 void print_times_table(int n)
 {
-	int a = 0; /* factor */
-	int b; /* count  */
-	int c; /* computed value */
+	int a = 0, b, c;
 
 	if (n > 15 || n < 0)
 		return;
-
 	while (a <= n)
 	{
 		b = 0;
 		while (b <= n)
 		{
 			c = a * b;
-			if (c > 99){
+			if (c > 99)
+			{
 				_putchar(c / 100 + '0');
 				_putchar((c / 10 % 10) + '0');
 				_putchar(c % 10 + '0');
 			}
-			else if (c > 9){
+			else if (c > 9)
+			{
 				_putchar(' ');
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
 			}
-			else if (b != 0){
+			else if (b != 0)
+			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
 			}
 			else
 				_putchar(c + '0');
-			if (b != n){
+			if (b != n)
+			{
 				_putchar(',');
 				_putchar(' ');
 			}
