823111dcea734e2eb383437de9021c62867fd779
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-23 18:22:53 -0800

    fix 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,28 +15,10 @@ void print_number(int n)
 		n = -n;
 		_putchar(45);
 	}
-	if (n > 1000)
+	
+	if (i / 10)
 	{
-		th = n / 1000;
-		h = n / 100 % 10;
-		t = n / 10 % 10;
-		d = n % 1000;
-		_putchar(th + '0');
+		print_number(i / 10);
 	}
-	else if (n > 100)
-	{
-		h = n / 100;
-		t = n / 10 % 10;
-		d = n % 100;
-		_putchar(h + '0');
-	}
-	else if (n > 10)
-	{
-		t = n / 10;
-		d = n % 10;
-		_putchar(t + '0');
-	}
-	else
-		d = n % 10;
-	_putchar(d + '0');
+	_putchar(i % 10 + '0');
 }
