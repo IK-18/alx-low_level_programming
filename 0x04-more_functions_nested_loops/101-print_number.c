cd5031a538500372870d2f11f5a9b6eded21bda0
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-02-23 18:27:41 -0800

    fix 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,35 +8,15 @@
 
 void print_number(int n)
 {
-	int th, h, t, d;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		n = -n;
 		_putchar(45);
+		x = -n;
 	}
-	if (n > 1000)
-        {
-                th = n / 1000;
-                h = (n / 100) % 10;
-                t = (n / 10) % 10;
-                d = n % 1000;
-                _putchar(th + '0');
-        }
-        else if (n > 100)
-        {
-                h = n / 100;
-                t = (n / 10) % 10;
-                d = n % 100;
-                _putchar(h + '0');
-        }
-        else if (n > 10)
-        {
-                t = n / 10;
-                d = n % 10;
-                _putchar(t + '0');
-        }
-        else
-                d = n % 10;
-        _putchar(d + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
