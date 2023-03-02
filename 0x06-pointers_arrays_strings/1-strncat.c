88a84aaff98e5f460b88e6c118d1cd781f15fd3f
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-03-01 18:37:33 -0800

    fixes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,12 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
-	for (j = 0; j < n; j++)
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		if (src[j] != '\0')
-		{
-			dest[i] = src[j];
-		}
+		dest[i] = src[j];
 		i++;
 	}
 	dest[i] = '\0';
