e70395ae9f3b725010f6a7f025e69a4f95d58576
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-03-13 15:51:53 -0700

    fix 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,13 +19,11 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	len = 0;
 	if (s1 == NULL)
-	{
-		s1 = '\0';
-	}
+		s1[0] = '\0';
 	if (s2 == NULL)
-	{
-		s2 = '\0';
-	}
+		s2[0] = '\0';
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	while (s1[i])
 		i++;
 	while (s2[j])
