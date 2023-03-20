3aebe09b95eec1972f88cd8b3452f57b2693be64
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-03-20 07:16:41 -0700

    finished tasks

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from, followed by a new line
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line
  *
  * Return: 0 on succes 1 on failure
  */
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
