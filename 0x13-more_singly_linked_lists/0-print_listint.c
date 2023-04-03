7f069ac030f950cd0a7b5648ab8f9cfe631ac41e
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-04-03 15:42:37 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: list
+ *
+ * Return: size of list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
