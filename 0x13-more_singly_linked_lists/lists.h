0e43adc72fe6b953cc3216b9dfe786db1a67744a
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-04-03 16:14:17 -0700

    fix 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,5 @@
-#ifndef "LISTS_H"
-#define "LISTS_H'
-
-size_t print_listint(const listint_t *h);
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct listint_s - singly linked list
@@ -16,5 +14,7 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
