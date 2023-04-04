80041e0134f77a145ab36b74e7094421d0eb8343
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-04-04 12:31:07 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sums all of the data in a list
+ * @head: first node in list
+ *
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return(0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
