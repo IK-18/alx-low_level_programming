3440f04b131db0bfb18571ee1e58df19ab261cf0
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-04-10 18:15:41 -0700

    fix task 4 betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 0 at a given index
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: number
  * @index: index of bit
  *
@@ -10,8 +10,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	        if (index > sizeof(n) * 8)
-			return (-1);
-		*n &= ~(1 << index);
-		return (1);
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
