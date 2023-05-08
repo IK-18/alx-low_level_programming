dadba0fda64a194da7ea932babd51c0ed861008d
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-05-08 16:29:16 -0700

    fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t oFile, rFile, wFile;
 	char *buff;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
