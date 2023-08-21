2575ef329c0cb4594d0efd0cdf5feef2880c27ca
IK-18 <ik.arigbe.osula@gmail.com>
2023-08-22 00:22:41 +0100

    Initialize files

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if it is an alphabet, 0 if not.
+ * @c: char type letter
+ * Return: 1 if alphabet, 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
